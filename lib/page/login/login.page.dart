cf1298926fc3cd722df89d240516d441a69bc622
Cyenoch <1759761439@qq.com>
2020-02-26 19:20:25 +0800

    重写启动页

---
+++ lib/page/login/login.page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: LoginForm(),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  LoginForm({Key key}) : super(key: key);
+
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
